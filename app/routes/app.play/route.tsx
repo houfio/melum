a835fe3a67b81a94c98e43fd96ad0678462f0187
houfio <github@houf.io>
2024-03-31 12:17:16 +0200

    Add track fetch pagination

--- app/routes/app.play/route.tsx
+++ app/routes/app.play/route.tsx
@@ -1,19 +1,39 @@
 import type { ClientLoaderFunctionArgs } from '@remix-run/react';
 import { useLoaderData } from '@remix-run/react';
-import type { Market } from '@spotify/web-api-ts-sdk';
+import type { Market, PlaylistedTrack, SpotifyApi, Track } from '@spotify/web-api-ts-sdk';
 
 import { Button } from '~/components/form/Button';
 import { usePlayer } from '~/hooks/usePlayer';
 import { getSpotify } from '~/utils/getSpotify';
+
+async function getPlaylist(sdk: SpotifyApi, market: Market, playlist: string, offset = 0, tracks: PlaylistedTrack<Track>[] = []) {
+  const fetched = await sdk.playlists.getPlaylistItems(
+    playlist,
+    market,
+    'items.track(id,uri,name,preview_url,is_playable,album(id,images,name),artists(id,name)),next,limit',
+    undefined,
+    offset
+  );
+  const result = [
+    ...tracks,
+    ...fetched.items
+  ];
+
+  if (!fetched.next) {
+    return result;
+  }
+
+  return getPlaylist(sdk, market, playlist, offset + fetched.limit, result);
+}
 
 export const clientLoader = async ({ request }: ClientLoaderFunctionArgs) => {
   const sdk = await getSpotify();
   const url = new URL(request.url);
   const country = url.searchParams.get('country');
   const playlists = url.searchParams.getAll('playlist');
-  const data = await Promise.all(playlists.map((playlist) => sdk.playlists.getPlaylistItems(playlist, country as Market, 'items.track(id,uri,name,preview_url,is_playable,album(id,images,name),artists(id,name)),next,offset')));
+  const data = await Promise.all(playlists.map((playlist) => getPlaylist(sdk, country as Market, playlist)));
 
-  return data.map((d) => d.items).flat();
+  return data.flat();
 };
 
 export default function Play() {
