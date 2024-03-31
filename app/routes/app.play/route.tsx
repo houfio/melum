cc04167a4fb11715dd7a6287da535eeabf002772
houfio <github@houf.io>
2024-03-31 13:10:06 +0200

    Add basic track guessing

--- app/routes/app.play/route.tsx
+++ app/routes/app.play/route.tsx
@@ -1,10 +1,13 @@
-import type { ClientLoaderFunctionArgs } from '@remix-run/react';
-import { useLoaderData } from '@remix-run/react';
+import type { ClientActionFunctionArgs, ClientLoaderFunctionArgs } from '@remix-run/react';
+import { useFetcher, useLoaderData } from '@remix-run/react';
 import type { Market, PlaylistedTrack, SpotifyApi, Track } from '@spotify/web-api-ts-sdk';
+import Fuse from 'fuse.js';
+import { useEffect, useState } from 'react';
 
 import { Button } from '~/components/form/Button';
 import { usePlayer } from '~/hooks/usePlayer';
 import { getSpotify } from '~/utils/getSpotify';
+import { wait } from '~/utils/wait';
 
 async function getPlaylist(sdk: SpotifyApi, market: Market, playlist: string, offset = 0, tracks: PlaylistedTrack<Track>[] = []) {
   const fetched = await sdk.playlists.getPlaylistItems(
@@ -36,18 +39,77 @@ export const clientLoader = async ({ request }: ClientLoaderFunctionArgs) => {
   return data.flat();
 };
 
+export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
+  const sdk = await getSpotify();
+  const data = await request.formData();
+
+  const deviceId = data.get('deviceId');
+  const uri = data.get('uri');
+
+  if (typeof deviceId !== 'string' || typeof uri !== 'string') {
+    throw new Error();
+  }
+
+  await sdk.player.startResumePlayback(deviceId, undefined, [uri]);
+  await wait(1000);
+  await sdk.player.pausePlayback(deviceId);
+
+  return { ok: true };
+};
+
+export const shouldRevalidate = () => false;
+
 export default function Play() {
   const tracks = useLoaderData<typeof clientLoader>();
+  const [fuse] = useState(() => new Fuse(tracks, {
+    keys: ['track.name'],
+    minMatchCharLength: 3,
+    threshold: .1
+  }));
   const [, , deviceId] = usePlayer();
+  const [track, setTrack] = useState<PlaylistedTrack<Track>>();
+  const [search, setSearch] = useState('');
+  const fetcher = useFetcher();
 
-  return (
-    <div>
-      {tracks.map((track) => (
-        <div key={track.track.id}>
-          <Button text="Play"/>
-          {track.track.artists.map((a) => a.name).join(', ')} - {track.track.name}
-        </div>
+  useEffect(() => {
+    if (!deviceId || !track) {
+      return;
+    }
+
+    const data = new FormData();
+
+    data.set('deviceId', deviceId);
+    data.set('uri', track.track.uri);
+
+    fetcher.submit(data, {
+      method: 'post'
+    });
+  }, [deviceId, track]);
+
+  function nextTrack() {
+    setSearch('');
+    setTrack(tracks[Math.floor(Math.random() * tracks.length)]);
+  }
+
+  return track ? (
+    <>
+      <input value={search} onChange={(e) => setSearch(e.target.value)}/>
+      {fuse.search(search).map(({ item }) => (
+        <Button
+          key={item.track.id}
+          text={item.track.name}
+          onClick={() => {
+            if (track.track.id === item.track.id) {
+              nextTrack();
+            }
+          }}
+        />
       ))}
-    </div>
+    </>
+  ) : (
+    <Button
+      text="Start first track"
+      onClick={() => nextTrack()}
+    />
   );
 }
